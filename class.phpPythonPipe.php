dc5a5155190af0604bfacd424a552bbe8c439acb
Filipi Vianna <filipi@pucrs.br>
2019-08-28 21:57:44 -0300

    Update class.phpPythonPipe.php
    Add a list  of commands to black list

--- class.phpPythonPipe.php
+++ class.phpPythonPipe.php
@@ -8,6 +8,29 @@ class phpPythonPipe{
   public $code = '';
   public $output = '';
 
+  /*
+  https://www.owasp.org/index.php/Testing_for_Command_Injection_(OTG-INPVAL-013)
+  black list system near commands:
+  PYTHON
+  exec
+  eval
+  os.system
+  os.popen
+  subprocess.popen
+  subprocess.call
+  
+  PHP
+  system
+  shell_exec
+  exec
+  proc_open
+  eval
+  passthru
+  proc_open
+  expect_open
+  ssh2_exec
+  popen
+  */
   public function exec() {
     $command = $this->kernelPath . " -c '" . $this->code . "'";
     $this->output = `$command`;
